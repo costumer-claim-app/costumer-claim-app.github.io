0111c6d91823bfbd5bb58606cde95471c2f9fe95
costumer-claim-app <kakamaamamamkskssk@gmail.com>
2025-04-07 10:47:42 +0700

    Add files via upload

---
+++ test64.js
@@ -0,0 +1,164 @@
+'use strict';
+
+var Base64 = {
+  // private property
+  _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+  // public method for encoding
+  encode: function (input) {
+    var output = "";
+    var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+    var i = 0;
+    input = Base64._utf8_encode(input);
+    while (i < input.length) {
+      chr1 = input.charCodeAt(i++);
+      chr2 = input.charCodeAt(i++);
+      chr3 = input.charCodeAt(i++);
+      enc1 = chr1 >> 2;
+      enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+      enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+      enc4 = chr3 & 63;
+      if (isNaN(chr2)) {
+        enc3 = enc4 = 64;
+      } else if (isNaN(chr3)) {
+        enc4 = 64;
+      }
+      output = output +
+        this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+        this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+    }
+    return output;
+  },
+  // public method for decoding
+  decode: function (input) {
+    var output = "";
+    var chr1, chr2, chr3;
+    var enc1, enc2, enc3, enc4;
+    var i = 0;
+    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+    while (i < input.length) {
+      enc1 = this._keyStr.indexOf(input.charAt(i++));
+      enc2 = this._keyStr.indexOf(input.charAt(i++));
+      enc3 = this._keyStr.indexOf(input.charAt(i++));
+      enc4 = this._keyStr.indexOf(input.charAt(i++));
+      chr1 = (enc1 << 2) | (enc2 >> 4);
+      chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+      chr3 = ((enc3 & 3) << 6) | enc4;
+      output = output + String.fromCharCode(chr1);
+      if (enc3 != 64) {
+        output = output + String.fromCharCode(chr2);
+      }
+      if (enc4 != 64) {
+        output = output + String.fromCharCode(chr3);
+      }
+    }
+    output = Base64._utf8_decode(output);
+    return output;
+  },
+  // private method for UTF-8 encoding
+  _utf8_encode: function (string) {
+    string = string.replace(/\r\n/g, "\n");
+    var utftext = "";
+    for (var n = 0; n < string.length; n++) {
+      var c = string.charCodeAt(n);
+      if (c < 128) {
+        utftext += String.fromCharCode(c);
+      }
+      else if ((c > 127) && (c < 2048)) {
+        utftext += String.fromCharCode((c >> 6) | 192);
+        utftext += String.fromCharCode((c & 63) | 128);
+      }
+      else {
+        utftext += String.fromCharCode((c >> 12) | 224);
+        utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+        utftext += String.fromCharCode((c & 63) | 128);
+      }
+    }
+    return utftext;
+  },
+  // private method for UTF-8 decoding
+  _utf8_decode: function (utftext) {
+    var string = "";
+    var i = 0;
+    var c = 0;
+    var c2 = 0;
+    var c3 = 0;
+    while (i < utftext.length) {
+      c = utftext.charCodeAt(i);
+      if (c < 128) {
+        string += String.fromCharCode(c);
+        i++;
+      }
+      else if ((c > 191) && (c < 224)) {
+        c2 = utftext.charCodeAt(i + 1);
+        string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+        i += 2;
+      }
+      else {
+        c2 = utftext.charCodeAt(i + 1);
+        c3 = utftext.charCodeAt(i + 2);
+        string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+        i += 3;
+      }
+    }
+    return string;
+  }
+};
+
+// var x = prompt('enter text: ');
+// Base64.encode(x);
+// alert(x);
+
+// console.log(Base64);
+
+// var y = Base64.encode('anmsdjinami21312312');
+
+// console.log('test encode 1: ' + y);
+
+// var a = Base64.encode('itrokmg weiuj893j');
+
+// console.log('test encode 2: ' + a);
+
+// var x = Base64.decode('YW5tc2RqaW5hbWkyMTMxMjMxMg==');
+
+// console.log('test decode 0: ' + x);
+
+// var z = Base64.decode(y);
+
+// console.log('test decode: ' + z);
+
+
+// var testInput = 'jrikiosmdf389ujr34imf';
+// var testInput = prompt('enter testInput: ');
+// console.log('testInput: ' + testInput);
+
+// var testEncode = Base64.encode(testInput);
+// console.log('testEncode: ' + testEncode);
+
+// var testDecode = Base64.decode(testEncode);
+// console.log('testDecode: ' + testDecode);
+
+// var testInput = prompt('enter testInput: ');
+// console.log('plain testInput: ' + testInput);
+
+// testInput = Base64.encode(testInput);
+// console.log('encoded testInput: ' + testInput);
+
+var x = location.href;
+
+var y = x.indexOf('#');
+
+// console.log(y);
+
+var z = x.slice(y + 1);
+
+// console.log(z);
+
+z = Base64.encode(z);
+
+console.log(z);
+
+var a = 'SGVsbG8='
+
+var b = Base64.decode(a);
+
+console.log(b);
